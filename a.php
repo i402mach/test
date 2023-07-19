ea56fd06afa173ff90f8c0a15e721e690caacbc7
i402mach <138637114+i402mach@users.noreply.github.com>
2023-07-19 10:46:15 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,10 @@
 #!/usr/bin/php
-<?php
+<?php 
   foreach(file('/usr/share/dict/words') as $line){
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-  }
+    // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+
+	  if(strpos($line, "xy") !== false){
+	  echo "$line";
+	  }
+  } 
 ?>
