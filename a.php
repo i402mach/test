1270f391142a3fe3b03f4fbce3e467f5abe6bee9
i402mach <138637114+i402mach@users.noreply.github.com>
2023-07-25 11:54:38 +0900

    Update a.php

--- a.php
+++ a.php
@@ -3,8 +3,8 @@
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
 
-	  if(strpos($line, "xy") !== false){
-	  echo "$line";
-	  }
+	if(strpos($line, "xy") !== false){
+	echo "$line";
+	}
   } 
 ?>
